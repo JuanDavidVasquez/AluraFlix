fa5a7482508eb7cdbdbdb155edf89950c02ec7bb
JuanDavidVasquez <67479047+JuanDavidVasquez@users.noreply.github.com>
2023-08-10 20:23:11 -0500

    2023

--- AluraFlixClient/src/components/layouts/Header.jsx
+++ AluraFlixClient/src/components/layouts/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from './Nav'
+import Sidebar from './Sidebar'
 
 export default function Header() {
   return (
     <div>
       <Nav/>
+      <Sidebar/>
     </div>
   )
 }
---
+++ AluraFlixClient/src/components/layouts/Sidebar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <div className='sidebar'>
+        
+        <div className='data_sidebar'>
+            <button>Front End</button>
+            <h2>Challenge React</h2>
+            <p>
+                Este challenge es una forma de aprendizaje.
+                Es un mecanismo donde podrás comprometerte en la 
+                resolución de un problema para poder aplicar todos
+                conocimientos adquiridos en la formación React.
+            </p>
+        </div>
+        <div className='video_sidebar'>
+            Video
+        </div>
+    </div>
+  )
+}
